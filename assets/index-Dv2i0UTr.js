b3864dde1b182221e065fed8ce1679cece9ced5b
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-04-19 10:36:28 +0900

    update

--- assets/index-Dv2i0UTr.js
+++ assets/index-Dv2i0UTr.js
@@ -4729,19 +4729,19 @@ const $n = "/logo.png",
   $c = {
     class: "sr-only",
   },
-  tn = "TBA",
+  tn = "0xcomingsoon",
   Hc = {
     __name: "App",
     setup(e) {
       const t = [
           {
             label: "Network",
-            value: "Solana Mainline",
+            value: "Ethereum",
           },
-          {
-            label: "Launchpad",
-            value: "Pump.fun Origin",
-          },
+          // {
+          //   label: "Launchpad",
+          //   value: "Pump.fun Origin",
+          // },
           {
             label: "Protocol",
             value: "Steady Face Runtime",
@@ -4750,15 +4750,22 @@ const $n = "/logo.png",
         n = [
           {
             platform: "twitter",
-            label: "Twitter",
-            url: "https://x.com/SPACEX4200_SOL",
+            label: "Twitter", 
+            url: "https://x.com/SPCX4200_x",
             iconSvg:
               '<svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M18.901 1.153h3.68l-8.039 9.188L24 22.847h-7.406l-5.8-7.584-6.634 7.584H.479l8.597-9.826L0 1.154h7.594l5.243 6.932L18.901 1.153Zm-1.29 19.494h2.038L6.486 3.24H4.298l13.313 17.407Z"/></svg>',
           },
           {
             platform: "telegram",
-            label: "Telegram",
-            url: "https://dexscreener.com/solana/TBA",
+            label: "telegram",
+            url: "https://t.me/SPCX4200",
+            iconSvg:
+              '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="white"><path d="M23.91 3.79L20.3 20.84c-.25 1.21-.98 1.5-2 .94l-5.5-4.07-2.66 2.57c-.3.3-.55.56-1.1.56-.72 0-.6-.27-.84-.95L6.3 13.7l-5.45-1.7c-1.18-.35-1.19-1.16.26-1.75l21.26-8.2c.97-.43 1.9.24 1.53 1.73z"/></svg>',
+          },
+          {
+            platform: "dexscreener",
+            label: "dexscreener",
+            url: "https://dexscreener.com/ethereum/0xcomingsoon",
             iconSvg:
               '<svg viewBox="0 0 252 300" aria-hidden="true"><path fill="currentColor" d="M151.818 106.866c9.177-4.576 20.854-11.312 32.545-20.541 2.465 5.119 2.735 9.586 1.465 13.193-.9 2.542-2.596 4.753-4.826 6.512-2.415 1.901-5.431 3.285-8.765 4.033-6.326 1.425-13.712.593-20.419-3.197m1.591 46.886l12.148 7.017c-24.804 13.902-31.547 39.716-39.557 64.859-8.009-25.143-14.753-50.957-39.556-64.859l12.148-7.017a5.95 5.95 0 003.84-5.845c-1.113-23.547 5.245-33.96 13.821-40.498 3.076-2.342 6.434-3.518 9.747-3.518s6.671 1.176 9.748 3.518c8.576 6.538 14.934 16.951 13.821 40.498a5.95 5.95 0 003.84 5.845zM126 0c14.042.377 28.119 3.103 40.336 8.406 8.46 3.677 16.354 8.534 23.502 14.342 3.228 2.622 5.886 5.155 8.814 8.071 7.897.273 19.438-8.5 24.796-16.709-9.221 30.23-51.299 65.929-80.43 79.589-.012-.005-.02-.012-.029-.018-5.228-3.992-11.108-5.988-16.989-5.988s-11.76 1.996-16.988 5.988c-.009.005-.017.014-.029.018-29.132-13.66-71.209-49.359-80.43-79.589 5.357 8.209 16.898 16.982 24.795 16.709 2.929-2.915 5.587-5.449 8.814-8.071C69.31 16.94 77.204 12.083 85.664 8.406 97.882 3.103 111.959.377 126 0m-25.818 106.866c-9.176-4.576-20.854-11.312-32.544-20.541-2.465 5.119-2.735 9.586-1.466 13.193.901 2.542 2.597 4.753 4.826 6.512 2.416 1.901 5.432 3.285 8.766 4.033 6.326 1.425 13.711.593 20.418-3.197"/><path fill="currentColor" d="M197.167 75.016c6.436-6.495 12.107-13.684 16.667-20.099l2.316 4.359c7.456 14.917 11.33 29.774 11.33 46.494l-.016 26.532.14 13.754c.54 33.766 7.846 67.929 24.396 99.193l-34.627-27.922-24.501 39.759-25.74-24.231L126 299.604l-41.132-66.748-25.739 24.231-24.501-39.759L0 245.25c16.55-31.264 23.856-65.427 24.397-99.193l.14-13.754-.016-26.532c0-16.721 3.873-31.578 11.331-46.494l2.315-4.359c4.56 6.415 10.23 13.603 16.667 20.099l-2.01 4.175c-3.905 8.109-5.198 17.176-2.156 25.799 1.961 5.554 5.54 10.317 10.154 13.953 4.48 3.531 9.782 5.911 15.333 7.161 3.616.814 7.3 1.149 10.96 1.035-.854 4.841-1.227 9.862-1.251 14.978L53.2 160.984l25.206 14.129a41.926 41.926 0 015.734 3.869c20.781 18.658 33.275 73.855 41.861 100.816 8.587-26.961 21.08-82.158 41.862-100.816a41.865 41.865 0 015.734-3.869l25.206-14.129-32.665-18.866c-.024-5.116-.397-10.137-1.251-14.978 3.66.114 7.344-.221 10.96-1.035 5.551-1.25 10.854-3.63 15.333-7.161 4.613-3.636 8.193-8.399 10.153-13.953 3.043-8.623 1.749-17.689-2.155-25.799l-2.01-4.175z"/></svg>',
           },
@@ -5181,7 +5188,7 @@ const $n = "/logo.png",
                           {
                             class: "brand-overline",
                           },
-                          "SPACEX4200 $SPCX4200"
+                          "SPACEX4200 $SPACEX"
                         ),
                         g("h1", null, "SPACEX4200"),
                       ]),
@@ -5269,7 +5276,7 @@ const $n = "/logo.png",
                           {
                             class: "hero-tag",
                           },
-                          "SOLANA $SPCX4200 CENTER"
+                          "SOLANA $SPACEX CENTER"
                         ),
                         g(
                           "h2",
@@ -5279,12 +5286,12 @@ const $n = "/logo.png",
                         g(
                           "p",
                           null,
-                          " SPACEX4200, with ticker $SPCX4200, is based on SpaceX's pre-IPO stock, currently trading at $116, and taps into the memetic love of Asuka and the world’s answer of '42.' Built on blockchain technology, $SPCX4200 brings the excitement of SpaceX’s $11.9 billion funding journey across 30 rounds directly to the public, bypassing traditional investment barriers. "
+                          " SPACEX4200, with ticker $SPACEX, is based on SpaceX's pre-IPO stock, currently trading at $116, and taps into the memetic love of Asuka and the world’s answer of '42.' Built on blockchain technology, $SPACEX brings the excitement of SpaceX’s $11.9 billion funding journey across 30 rounds directly to the public, bypassing traditional investment barriers. "
                         ),
                         g(
                           "p",
                           null,
-                          "With an eye on a potential $4,200 billion valuation, $SPCX4200 serves as a decentralized reflection of SpaceX’s growth, allowing enthusiasts to engage in the company’s journey through a unique, crypto-fueled lens."
+                          "With an eye on a potential $1 billion valuation, $SPACEX serves as a decentralized reflection of SpaceX’s growth, allowing enthusiasts to engage in the company’s journey through a unique, crypto-fueled lens."
                         ),
                       ],
                       -1
